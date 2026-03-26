40472173b9a935f4601743201bf4e7b7728f969e
monu242 <monucut657@gmail.com>
2026-03-26 11:52:36 +0530

    writed code for managing files

--- utils/file_manager.py
+++ utils/file_manager.py
@@ -1,15 +1,34 @@
 import os #library of operating system 
-
+from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDERS = 'uploads'
 
 
-def save_files(file):
-    pass
+def save_files(file,folder):
+    filename = secure_filename(file.filename)
+    path = os.path.join(filename,folder)
+    file.save(path)
+    return filename
 
 
-def upload_files(file):
-    pass
+def upload_files(folder):
+    if not os.path.exists(folder):
 
-def delete_files(file):
-    pass
+        return[]
+    files = []
+    for f in os.listdir(folder):
+        if f.endswith(".pdf"):
+            path = os.path.join(folder, f)
+            size = os.path.getsize(path)  # size in bytes
+            files.append({
+                "name": f,
+                "size": f"{round(size / 1024 / 1024, 1)} MB"
+            })
+    return files
+
+
+def delete_files(filename,folder):
+    path = os.path.join(filename,folder)
+    if os.path.exists(path):
+        os.remove(path)
+    
