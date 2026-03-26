72fac07bd742cd0f2f06f6d7646d582330b64444
monu242 <monucut657@gmail.com>
2026-03-26 11:09:15 +0530

    extract the text from the user uploads

--- utils/pdf_parser.py
+++ utils/pdf_parser.py
@@ -1,4 +1,8 @@
 import pdfplumber#a  python library which is used to extract text from the pdf
 
-def extract_text(filename):
-    pass
+def extract_text(pdf_path):
+    text = ""
+    with  pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            text+= page.extract_text()
+            return text [:12000]
